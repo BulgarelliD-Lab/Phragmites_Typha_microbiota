275e92549d894b78ffc6d5b8f42bac52e0ac7ed8
BulgarelliD-Lab <d.bulgarelli@dundee.ac.uk>
2018-07-06 16:24:12 +0100

    Add files via upload

---
+++ JH06_session_info0718.R
@@ -0,0 +1,52 @@
+R version 3.4.2 (2017-09-28)
+Platform: x86_64-w64-mingw32/x64 (64-bit)
+Running under: Windows >= 8 x64 (build 9200)
+
+Matrix products: default
+
+locale:
+[1] LC_COLLATE=Italian_Italy.1252  LC_CTYPE=Italian_Italy.1252   
+[3] LC_MONETARY=Italian_Italy.1252 LC_NUMERIC=C                  
+[5] LC_TIME=Italian_Italy.1252    
+
+attached base packages:
+ [1] grid      parallel  stats4    stats     graphics  grDevices utils     datasets 
+ [9] methods   base     
+
+other attached packages:
+ [1] ade4_1.7-8                 PMCMR_4.1                 
+ [3] VennDiagram_1.6.17         futile.logger_1.4.3       
+ [5] ape_5.0                    vegan_2.4-4               
+ [7] lattice_0.20-35            permute_0.9-4             
+ [9] ggplot2_2.2.1              DESeq2_1.18.1             
+[11] SummarizedExperiment_1.8.0 DelayedArray_0.4.1        
+[13] matrixStats_0.52.2         Biobase_2.38.0            
+[15] GenomicRanges_1.30.0       GenomeInfoDb_1.14.0       
+[17] IRanges_2.12.0             S4Vectors_0.16.0          
+[19] BiocGenerics_0.24.0        phyloseq_1.22.3           
+
+loaded via a namespace (and not attached):
+ [1] bit64_0.9-7             jsonlite_1.5            splines_3.4.2          
+ [4] foreach_1.4.3           Formula_1.2-2           latticeExtra_0.6-28    
+ [7] blob_1.1.0              GenomeInfoDbData_0.99.1 RSQLite_2.0            
+[10] backports_1.1.1         digest_0.6.12           RColorBrewer_1.1-2     
+[13] XVector_0.18.0          checkmate_1.8.5         colorspace_1.3-2       
+[16] htmltools_0.3.6         Matrix_1.2-11           plyr_1.8.4             
+[19] pkgconfig_2.0.1         XML_3.98-1.9            genefilter_1.60.0      
+[22] zlibbioc_1.24.0         xtable_1.8-2            scales_0.5.0           
+[25] BiocParallel_1.12.0     htmlTable_1.9           tibble_1.3.4           
+[28] annotate_1.56.1         mgcv_1.8-20             nnet_7.3-12            
+[31] lazyeval_0.2.1          survival_2.41-3         magrittr_1.5           
+[34] memoise_1.1.0           MASS_7.3-47             nlme_3.1-131           
+[37] foreign_0.8-69          tools_3.4.2             data.table_1.10.4-3    
+[40] stringr_1.2.0           munsell_0.4.3           locfit_1.5-9.1         
+[43] cluster_2.0.6           lambda.r_1.2            AnnotationDbi_1.40.0   
+[46] Biostrings_2.46.0       compiler_3.4.2          rlang_0.1.4            
+[49] rhdf5_2.22.0            RCurl_1.95-4.8          iterators_1.0.8        
+[52] biomformat_1.6.0        htmlwidgets_0.9         igraph_1.1.2           
+[55] bitops_1.0-6            base64enc_0.1-3         multtest_2.34.0        
+[58] gtable_0.2.0            codetools_0.2-15        DBI_0.7                
+[61] reshape2_1.4.2          gridExtra_2.3           knitr_1.17             
+[64] bit_1.1-12              Hmisc_4.0-3             futile.options_1.0.0   
+[67] stringi_1.1.5           Rcpp_0.12.13            geneplotter_1.56.0     
+[70] rpart_4.1-11            acepack_1.4.1          
